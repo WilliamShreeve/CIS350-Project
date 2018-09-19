be324dbb316a6614a48620d8213f2cabc6f3db9b
William <shreevew@mail.gvsu.edu>
2018-09-19 10:53:49 -0400

    Worked on GUI

--- GVSUDatabase/src/gvsuDatabase/DatabaseGUI.java
+++ GVSUDatabase/src/gvsuDatabase/DatabaseGUI.java
@@ -1,6 +1,59 @@
 package gvsuDatabase;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class DatabaseGUI {
+import javax.swing.JFrame;
+import javax.swing.*;
+
+public class DatabaseGUI extends JFrame implements ActionListener{
+
+	/* Holds JMenus.*/
+	private JMenuBar menus;
+	
+	/* JMenu for file options */
+	private JMenu fileMenu;
+	
+	/* JMenu for editing options. */
+	private JMenu editMenu;
+	
+	/* JMenu for different views. */
+	private JMenu viewMenu;
+	
+	/*JMenu Items*/
+	private JMenuItem saveSerial;
+	private JMenuItem loadSerial;
+	private JMenuItem addStudent;
+	private JMenuItem editStudent;
+	private JMenuItem viewStudent;
+	
+	
+	public DatabaseGUI() {
+		fileMenu = new JMenu("File");
+		editMenu = new JMenu("Edit");
+		viewMenu = new JMenu("View");
+		
+		saveSerial = new JMenuItem("Save");
+		loadSerial = new JMenuItem("Load");
+		addStudent = new JMenuItem("Add Student");
+		editStudent = new JMenuItem("Edit Student");
+		viewStudent = new JMenuItem("View Stuent");
+		
+		fileMenu.add(saveSerial);
+		fileMenu.add(loadSerial);
+		
+		editMenu.add(addStudent);
+		editMenu.add(editStudent);
+		
+		viewMenu.add(viewStudent);
+		
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- GVSUDatabase/src/gvsuDatabase/StudentTable.java
+++ GVSUDatabase/src/gvsuDatabase/StudentTable.java
@@ -1,5 +1,38 @@
 package gvsuDatabase;
 
-public class StudentTable {
+import javax.swing.table.AbstractTableModel;
+
+
+public class StudentTable extends AbstractTableModel{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String[] columnNames = {"Name",
+	                                "Major",
+	                                "Standing",
+	                                "G-Number",
+	                                "Exp. Grad"}; 
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return columnNames.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
